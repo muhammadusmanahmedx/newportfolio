972859b3a1c7c0d920123a937248af3e06a921fb
Muhammad Usman Ahmed <usmanahmed3377@gmail.com>
2025-08-19 21:25:29 -0700

    without gravity

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Gravity, MatterBody } from "./ui/gravity";
-
 function Preview() {
   const handleDownloadCV = () => {
     // Replace with your actual CV file path
@@ -30,64 +28,6 @@ function Preview() {
           Download CV
         </button>
       </div>
-
-      <Gravity gravity={{ x: 0, y: 1 }} className="w-full h-full">
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="30%"
-          y="10%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-600 text-white rounded-full hover:cursor-pointer px-8 py-4">
-            react
-          </div>
-        </MatterBody>
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="30%"
-          y="30%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-500 text-white rounded-full hover:cursor-grab px-8 py-4">
-            typescript
-          </div>
-        </MatterBody>
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="40%"
-          y="20%"
-          angle={10}
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-700 text-white rounded-full hover:cursor-grab px-8 py-4">
-            motion
-          </div>
-        </MatterBody>
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="75%"
-          y="10%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-800 text-white rounded-full hover:cursor-grab px-8 py-4">
-            tailwind
-          </div>
-        </MatterBody>
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="80%"
-          y="20%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-400 text-white rounded-full hover:cursor-grab px-8 py-4">
-            drei
-          </div>
-        </MatterBody>
-        <MatterBody
-          matterBodyOptions={{ friction: 0.5, restitution: 0.2 }}
-          x="50%"
-          y="10%"
-        >
-          <div className="text-xl sm:text-2xl md:text-3xl bg-pink-900 text-white rounded-full hover:cursor-grab px-8 py-4">
-            matter-js
-          </div>
-        </MatterBody>
-      </Gravity>
     </div>
   );
 }
